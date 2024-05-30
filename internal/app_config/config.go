85ea4e443618619dc58d5f7957ab8ddf5efff1be
Matthew Bridges <mattbridgesbusiness@gmail.com>
2024-05-30 09:40:51 -0700

    feat: add debug logs + knock secret

--- internal/app_config/config.go
+++ internal/app_config/config.go
@@ -3,6 +3,7 @@ package app_config
 import (
 	"fmt"
 	"log"
+	"log/slog"
 
 	"github.com/caarlos0/env/v11"
 	"github.com/joho/godotenv"
@@ -13,8 +14,14 @@ type CoinbaseConfig struct {
 	Secret     string `env:"CB_API_PRIVACY_KEY,required"`
 }
 
+type KnockConfig struct {
+	Secret string `env:"KNOCK_SECRET_KEY,required"`
+}
+
 type AppConfig struct {
+	EnableDebugLogs     bool `env:"ENABLE_DEBUG_LOGS"`
 	Coinbase            CoinbaseConfig
+	Knock               KnockConfig
 	ShouldMakeMinTrades bool `env:"SHOULD_MAKE_MIN_TRADES"`
 	ForceSell           bool `env:"FORCE_SELL"`
 	ForceBuy            bool `env:"FORCE_BUY"`
@@ -32,6 +39,10 @@ func LoadConfig() AppConfig {
 		log.Fatalf("Error mounting config: %v", err)
 	}
 
+	if cfg.EnableDebugLogs {
+		slog.SetLogLoggerLevel(slog.LevelDebug)
+	}
+
 	return cfg
 }
 
@@ -44,9 +55,11 @@ func obfuscateSecret(s string, reveal_n int) string {
 
 func (c AppConfig) String() string {
 	return fmt.Sprintf(
-		"AppConfig(Coinbase(ApiKeyName: %s, Secret: %s), ShouldMakeMinTrades: %t, ForceSell: %t, ForceBuy: %t)",
+		"AppConfig(EnableDebugLogs: %t, Coinbase(ApiKeyName: %s, Secret: %s), Knock(Secret: %s) ShouldMakeMinTrades: %t, ForceSell: %t, ForceBuy: %t)",
+		c.EnableDebugLogs,
 		obfuscateSecret(c.Coinbase.ApiKeyName, 3),
 		obfuscateSecret(c.Coinbase.Secret, 5),
+		obfuscateSecret(c.Knock.Secret, 12),
 		c.ShouldMakeMinTrades,
 		c.ForceSell,
 		c.ForceBuy,
